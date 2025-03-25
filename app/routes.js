7361e372fcecf4e5151c02e4f7ad0b10a2b8434c
robincoxNHS <90314736+robincoxNHS@users.noreply.github.com>
2025-03-25 13:05:43 +0000

    Intial commit

---
+++ app/routes.js
@@ -0,0 +1,12 @@
+//
+// For guidance on how to create routes see:
+// https://prototype-kit.service.gov.uk/docs/create-routes
+//
+
+const govukPrototypeKit = require('govuk-prototype-kit')
+const router = govukPrototypeKit.requests.setupRouter()
+
+const radioButtonRedirect = require('radio-button-redirect')
+router.use(radioButtonRedirect)
+
+// Add your routes here
