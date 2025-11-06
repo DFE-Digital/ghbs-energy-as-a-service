d3536d24ed3d96b3fc28918d967e7ac69cafb467
Rich-Cooley <91479782+Rich-Cooley@users.noreply.github.com>
2025-11-06 13:55:56 +0000

    Moving to routes.js part 1
    - added sprint-22 routes
    - main routes.js updated to include sprint-22 routes
    - index.html sets journeyType to MAT
    - before-you-start uses routing to dfe-sign-in-email
    - dfe-sign-in-email uses routing to dfe-sign-in-password
    - dfe-sign-in-password uses routing to which-school-are-you-buying-for or single-school-authorisation

---
+++ app/routes/sprint-22.js
@@ -0,0 +1,38 @@
+module.exports = function (router) {
+
+  var version = "sprint-22";
+
+  router.get('/' + version + '/before-you-start', function (req, res) {
+    res.render(version + '/before-you-start', {})
+  })
+
+  router.post('/' + version + '/before-you-start', function (req, res) {
+    res.redirect('/' + version + '/dfe-sign-in-email')
+  })
+
+
+  router.get('/' + version + '/dfe-sign-in-email', function (req, res) {
+    res.render(version + '/dfe-sign-in-email', {})
+  })
+
+  router.post('/' + version + '/dfe-sign-in-email', function (req, res) {
+    res.redirect('/' + version + '/dfe-sign-in-password')
+  })
+
+
+  router.get('/' + version + '/dfe-sign-in-password', function (req, res) {
+    res.render(version + '/dfe-sign-in-password', {})
+  })
+
+  router.post('/' + version + '/dfe-sign-in-password', function (req, res) {
+
+    if (req.session.data['journeyType'] == 'MAT') {
+      res.redirect('/' + version + '/which-school-are-you-buying-for')
+    } else {
+      res.redirect('/' + version + '/single-school-authorisation')
+    }
+
+  })
+
+
+}
